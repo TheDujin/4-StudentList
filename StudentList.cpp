346ee1c16afccb57f53f6699f0aeeac8b9bce62a
U-BSD\293359 <293359@SHS-10D8A831B.beaverton.k12.or.us>
2016-10-07 14:31:22 -0700

    Can accept commands and created addStudent() function which is not complete.

--- StudentList.cpp
+++ StudentList.cpp
@@ -10,20 +10,62 @@ struct Student {
   float id;
   float gpa;
 };
+Student addStudent();
 
+Student addStudent() {
+  Student newStudent;
+  char input[81];
+  cout << "Please enter the first name of the student." << endl << "First name: ";
+  cin >> newStudent.firstName;
+  cout << "Please enter the last name of the student." << endl << "Last name: ";
+  cin >> newStudent.lastName;
+  cout << "Please enter the student ID." << endl << "Student ID: ";
+  cin >> newStudent.id;
+
+
+  return newStudent;
+}
 
 int main() {
+  char input[81];
   vector<Student> sList;
+  vector<Student> * listPtr = & sList;
   char add[4] = {'A', 'D', 'D', '\0'};
   char print[6] = {'P', 'R', 'I', 'N', 'T', '\0'};
   char del[7] = {'D', 'E', 'L', 'E', 'T', 'E', '\0'};
-
+  char quit[5] = {'Q', 'U', 'I', 'T', '\0'};
+  bool running = true;
   
-  Student george;
-  cin >> george.name;
-  george.id = 1234;
-  george.gpa = 3.0;
-  cout << george.name << " ID: " << george.id << " GPA: " << george.gpa << endl;
+  while (running) {
+    cout << "Please type \"ADD\", \"PRINT\", \"DELETE\", or \"QUIT\" as commands then press Enter." << endl << "Input: ";
+    cin.getline(input, 80);
+    if (strcmp(input, add) == 0) {
+      cout << "Adding..." << endl;
+      addStudent();
+    }
+    else if (strcmp(input, print) == 0) {
+      cout << "Printing..." << endl;
+    }
+    else if (strcmp(input, del) == 0) {
+      cout << "Deleting..." << endl;
+    }
+    else if (strcmp(input, quit) == 0) {
+      cout << "Quitting..." << endl << "Program terminated. Thank you for your time.";
+      running = false;
+    }
+    else {
+      cout << "That input isn't valid. Please check your spelling and ensure that all letters are capitalized." << endl;
+    }
+    
+    
+  }
+  
+  
+  //Student george;
+  //cin >> george.name;
+  //george.id = 1234;
+  //george.gpa = 3.0;
+  //cout << george.name << " ID: " << george.id << " GPA: " << george.gpa << endl;
 
 
 
